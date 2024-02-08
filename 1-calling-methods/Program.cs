684311a07643df61c4b57ef94b7ae0b71ac3eb98
Amappola <anam.porrasp@gmail.com>
2024-02-08 07:42:41 -0500

    Learning how to use methods from the .NET Class Library

---
+++ 1-calling-methods/Program.cs
@@ -0,0 +1,10 @@
+﻿// Learning how to use methods from the .NET Class Library
+
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(-50, 0);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
