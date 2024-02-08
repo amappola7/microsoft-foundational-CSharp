d8fe8341ae1741dedb1a304271cb63d360f8546f
Amappola <anam.porrasp@gmail.com>
2024-02-08 07:46:25 -0500

    My very first C# project

---
+++ my-first-project/my-first-CSharp-project/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!. Hello, C#!");
+Console.WriteLine("To open a new C# console run the command: dotnet new console -o folderPath");
