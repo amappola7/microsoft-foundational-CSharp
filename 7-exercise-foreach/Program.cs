4b725ec274a41d1405f9909fd873fbb25701aa39
Amappola <anam.porrasp@gmail.com>
2024-02-08 07:45:26 -0500

    Learning to use the for each loop to iterate trough an array

---
+++ 7-exercise-foreach/Program.cs
@@ -0,0 +1,14 @@
+﻿// Learning to use the for each loop to iterate trough an array
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int total = 0;
+int bin = 0;
+
+foreach (int item in inventory)
+{
+  total += item;
+  bin++;
+  Console.WriteLine($"Bin {bin} = {item} items (Running total: {total})");
+}
+
+Console.WriteLine($"We have {total} items in inventory.");
