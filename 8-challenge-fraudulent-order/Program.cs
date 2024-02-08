d3d4fc370d3f776cdff3a924c0c49bf3444e38ea
Amappola <anam.porrasp@gmail.com>
2024-02-08 07:45:43 -0500

    Challenge Fraudulent Orders

---
+++ 8-challenge-fraudulent-order/Program.cs
@@ -0,0 +1,12 @@
+﻿// Challenge Fraudulent Orders
+
+string[] orders = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+Console.WriteLine("Potentially fraudulent orders: ");
+foreach(string order in orders)
+{
+  if(order.StartsWith("B"))
+  {
+    Console.WriteLine(order);
+  }
+}
