21c96a35e2a6e9dbfcf8ad69bafe60321a51ce2e
Amappola <anam.porrasp@gmail.com>
2024-02-21 07:45:45 -0500

    Exercise evaluate boolean expressions

---
+++ 13-exercise-boolean-expressions/Program.cs
@@ -0,0 +1,34 @@
+﻿// Exercise Evaluate Boolean Expressions
+
+// Exercise 1
+Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+string myValue2 = "A";
+Console.WriteLine(myValue2.Trim().ToLower() == "a");
+
+// Exercise 2
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+// string myValue = "a";
+Console.WriteLine(myValue != "a");
+
+// Exercise 3
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+
+// Exercise 4
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+
+// Exercise 5
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
