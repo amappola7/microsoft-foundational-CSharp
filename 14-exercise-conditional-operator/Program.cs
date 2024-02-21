97c03bc6723e16bad5ad3c69811ee17eaf6c0676
Amappola <anam.porrasp@gmail.com>
2024-02-21 07:46:57 -0500

    Exercise conditional operator

---
+++ 14-exercise-conditional-operator/Program.cs
@@ -0,0 +1,8 @@
+﻿//  Exercise conditional operator
+
+// Console.Write("What is the value of your purchase?");
+// int purchaseValue = Console.Read();
+int purchaseValue = 100000;
+int discount = purchaseValue >= 1000 ? 100 : 50;
+Console.WriteLine($"Your purchase is: {purchaseValue} dollars.");
+Console.WriteLine($"Your discount is: {discount} dollars.");
